365dabc65895ec4ccdbd580ba76354b767a35b1f
Stian Rossavik Sporaland <stian.sporaland@pm.me>
2023-11-19 10:32:28 +0100

    Update Cloudflare provider credentials

--- infrastructure/terraform/cloudflare/providers.tf
+++ infrastructure/terraform/cloudflare/providers.tf
@@ -1,4 +1,3 @@
 provider "cloudflare" {
-  email   = module.onepassword_item_cloudflare.fields["username"]
-  api_key = module.onepassword_item_cloudflare.fields["api-key"]
+  api_token = module.onepassword_item_cloudflare.fields["api-token"]
 }
