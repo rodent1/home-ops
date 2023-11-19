340a0fedbb41d04695a93aec75e96feba2ba5499
Stian Rossavik Sporaland <stian.sporaland@pm.me>
2023-11-19 09:59:39 +0100

    Update .gitignore and main.tf files

--- infrastructure/terraform/cloudflare/main.tf
+++ infrastructure/terraform/cloudflare/main.tf
@@ -1,10 +1,10 @@
 terraform {
-  cloud {
-    organization = ""
-    workspaces {
-      name = "home-cloudflare-provisioner"
-    }
-  }
+  # cloud {
+  #   organization = ""
+  #   workspaces {
+  #     name = ""
+  #   }
+  # }
 
   required_providers {
     cloudflare = {
@@ -16,4 +16,19 @@ terraform {
       version = "3.4.0"
     }
   }
+}
+
+# Obtain current home IP address
+data "http" "ipv4_lookup_raw" {
+  url = "http://ipv4.icanhazip.com"
+}
+
+module "onepassword_item_cloudflare" {
+  source = "github.com/bjw-s/terraform-1password-item?ref=main"
+  vault  = "Services"
+  item   = "Cloudflare"
+}
+
+data "cloudflare_zone" "domain" {
+  name = "rodent.cc"
 }
