c4d28984dd5fd9b26efd6e005d33ef15bf5020a4
Stian Rossavik Sporaland <stian.sporaland@pm.me>
2023-12-17 12:01:48 +0100

    Update OAuth2 configuration

--- kubernetes/main/apps/database/pgadmin/app/config/config_local.py
+++ kubernetes/main/apps/database/pgadmin/app/config/config_local.py
@@ -11,6 +11,6 @@ OAUTH2_CONFIG = [{
   'OAUTH2_AUTHORIZATION_URL': 'https://authentik.${SECRET_DOMAIN}/application/o/authorize/',
   'OAUTH2_USERINFO_ENDPOINT': 'https://authentik.${SECRET_DOMAIN}/application/o/userinfo/',
   'OAUTH2_SERVER_METADATA_URL': 'https://authentik.${SECRET_DOMAIN}/application/o/pgadmin/.well-known/openid-configuration',
-  'OAUTH_API_BASE_URL': 'https://authentik.${SECRET_DOMAIN}/',
-  'OAUTH_SCOPE': 'openid email profile'
+  'OAUTH2_API_BASE_URL': 'https://authentik.${SECRET_DOMAIN}/',
+  'OAUTH2_SCOPE': 'openid email profile'
 }]
