c7ea0470c1c31e439f5caf0f4d54c7db66c66ac4
Stian Rossavik Sporaland <stian.sporaland@pm.me>
2023-12-17 19:48:08 +0100

    Remove commented out files and disable CoreDNS in talconfig.yaml

--- kubernetes/main/talos/apply-clusterconfig.sh
+++ kubernetes/main/talos/apply-clusterconfig.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Deploy the configuration to the nodes
-talosctl apply-config -n 10.1.1.31 -f ./clusterconfig/main-node-1.rodent.casa.yaml --insecure
-talosctl apply-config -n 10.1.1.32 -f ./clusterconfig/main-node-2.rodent.casa.yaml --insecure
-talosctl apply-config -n 10.1.1.33 -f ./clusterconfig/main-node-3.rodent.casa.yaml --insecure
-talosctl apply-config -n 10.1.1.34 -f ./clusterconfig/main-worker-1.rodent.casa.yaml --insecure
-talosctl apply-config -n 10.1.1.35 -f ./clusterconfig/main-worker-2.rodent.casa.yaml --insecure
+talosctl apply-config -n 10.1.1.31 -f ./clusterconfig/main-node-1.rodent.casa.yaml
+talosctl apply-config -n 10.1.1.32 -f ./clusterconfig/main-node-2.rodent.casa.yaml
+talosctl apply-config -n 10.1.1.33 -f ./clusterconfig/main-node-3.rodent.casa.yaml
+talosctl apply-config -n 10.1.1.34 -f ./clusterconfig/main-worker-1.rodent.casa.yaml
+talosctl apply-config -n 10.1.1.35 -f ./clusterconfig/main-worker-2.rodent.casa.yaml
