3a2b3163721270a6eacda85483e4ecb1cecd3535
Stian Rossavik Sporaland <stian.sporaland@pm.me>
2023-12-17 17:48:51 +0100

    Update cilium

--- kubernetes/main/talos/deploy-integrations.sh
+++ kubernetes/main/talos/deploy-integrations.sh
@@ -6,7 +6,10 @@ if ! kubectl get ns system-controllers >/dev/null 2>&1; then
   kubectl create ns system-controllers
 fi
 
-kubectl kustomize --enable-helm ./cni | kubectl apply -f -
+rm -rf cni/charts
+envsubst < ../apps/kube-system/cilium/app/values.yaml > cni/values.yaml
+kustomize build --enable-helm cni | kubectl apply -f -
+rm cni/values.yaml
 rm -rf cni/charts
 
 kubectl kustomize --enable-helm ./kubelet-csr-approver | kubectl apply -f -
