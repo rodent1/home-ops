dee8355658bd44117109cd06cce3fac5940bf4f0
Stian Rossavik Sporaland <stian.sporaland@pm.me>
2024-01-27 18:40:38 +0100

    Update Rook and Flux configurations

--- kubernetes/main/talos/apply-clusterconfig.sh
+++ kubernetes/main/talos/apply-clusterconfig.sh
@@ -4,5 +4,5 @@
 talosctl apply-config -n 10.1.1.31 -f ./clusterconfig/main-node-1.rodent.casa.yaml
 talosctl apply-config -n 10.1.1.32 -f ./clusterconfig/main-node-2.rodent.casa.yaml
 talosctl apply-config -n 10.1.1.33 -f ./clusterconfig/main-node-3.rodent.casa.yaml
-talosctl apply-config -n 10.1.1.34 -f ./clusterconfig/main-worker-1.rodent.casa.yaml
-talosctl apply-config -n 10.1.1.35 -f ./clusterconfig/main-worker-2.rodent.casa.yaml
+talosctl apply-config -n 10.1.1.34 -f ./clusterconfig/main-node-4.rodent.casa.yaml
+talosctl apply-config -n 10.1.1.35 -f ./clusterconfig/main-node-5.rodent.casa.yaml
