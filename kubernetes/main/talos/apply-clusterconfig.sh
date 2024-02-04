d621a9828070e59f4d9cd644d37d2275a09dfab6
Stian Rossavik Sporaland <stian.sporaland@pm.me>
2024-02-04 15:57:07 +0100

    Update Taskfile.yaml and remove apply-clusterconfig.sh

--- kubernetes/main/talos/apply-clusterconfig.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Deploy the configuration to the nodes
-talosctl apply-config -n 10.1.1.31 -f ./clusterconfig/main-node-1.rodent.casa.yaml
-talosctl apply-config -n 10.1.1.32 -f ./clusterconfig/main-node-2.rodent.casa.yaml
-talosctl apply-config -n 10.1.1.33 -f ./clusterconfig/main-node-3.rodent.casa.yaml
-talosctl apply-config -n 10.1.1.34 -f ./clusterconfig/main-node-4.rodent.casa.yaml
-talosctl apply-config -n 10.1.1.35 -f ./clusterconfig/main-node-5.rodent.casa.yaml
