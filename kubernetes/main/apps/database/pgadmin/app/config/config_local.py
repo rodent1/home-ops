477a50c4b34628157f089171d02174ce0b79b888
Stian R. Sporaland <mail@stianrs.dev>
2024-03-16 21:30:32 +0100

    Update OAuth2 configuration in pgadmin config_local.py

--- kubernetes/main/apps/database/pgadmin/app/config/config_local.py
+++ kubernetes/main/apps/database/pgadmin/app/config/config_local.py
@@ -5,12 +5,12 @@ MASTER_PASSWORD_REQUIRED = False
 OAUTH2_CONFIG = [{
   'OAUTH2_NAME': 'authentik',
   'OAUTH2_DISPLAY_NAME': 'Authentik',
-  'OAUTH2_CLIENT_ID': '{{ .OAUTH2_CLIENT_ID }}',
-  'OAUTH2_CLIENT_SECRET': '{{ .OAUTH2_CLIENT_SECRET }}',
-  'OAUTH2_TOKEN_URL': 'https://authentik.${SECRET_DOMAIN}/application/o/token/',
-  'OAUTH2_AUTHORIZATION_URL': 'https://authentik.${SECRET_DOMAIN}/application/o/authorize/',
-  'OAUTH2_USERINFO_ENDPOINT': 'https://authentik.${SECRET_DOMAIN}/application/o/userinfo/',
-  'OAUTH2_SERVER_METADATA_URL': 'https://authentik.${SECRET_DOMAIN}/application/o/pgadmin/.well-known/openid-configuration',
-  'OAUTH2_API_BASE_URL': 'https://authentik.${SECRET_DOMAIN}/',
+  'OAUTH2_CLIENT_ID': '{{ .PGADMIN_OAUTH2_CLIENT_ID }}',
+  'OAUTH2_CLIENT_SECRET': '{{ .PGADMIN_OAUTH2_CLIENT_SECRET }}',
+  'OAUTH2_TOKEN_URL': '{{ .PGADMIN_OAUTH2_TOKEN_URL }}',
+  'OAUTH2_AUTHORIZATION_URL': '{{ .PGADMIN_OAUTH2_AUTHORIZATION_URL }}',
+  'OAUTH2_USERINFO_ENDPOINT': '{{ .PGADMIN_OAUTH2_USERINFO_ENDPOINT }}',
+  'OAUTH2_SERVER_METADATA_URL': '{{ .PGADMIN_OAUTH2_SERVER_METADATA_URL }}',
+  'OAUTH2_API_BASE_URL': '{{ .PGADMIN_OAUTH2_API_BASE_URL }}',
   'OAUTH2_SCOPE': 'openid email profile'
 }]
