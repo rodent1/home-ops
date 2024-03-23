4a35a49a0aa662ebb81ecb76829d5c340678f194
Stian R. Sporaland <mail@stianrs.dev>
2024-03-23 17:13:11 +0100

    Delete archive + terraform

--- terraform/rodent-cc/account.tf
+++
@@ -1,5 +0,0 @@
-resource "cloudflare_account" "main" {
-  name              = "Home-ops"
-  type              = "standard"
-  enforce_twofactor = false
-}
--- terraform/rodent-cc/providers.tf
+++
@@ -1,4 +0,0 @@
-provider "cloudflare" {
-  email   = module.onepassword_item_cloudflare.fields["CLOUDFLARE_EMAIL"]
-  api_key = module.onepassword_item_cloudflare.fields["CLOUDFLARE_API_KEY"]
-}
