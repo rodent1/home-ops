2424aede6371420779909059e297927d649600b8
Stian R. Sporaland <mail@stianrs.dev>
2024-05-04 11:07:45 +0200

    Move cross-seed, add autobrr

---
+++ kubernetes/main/apps/media/qbittorrent/tools/cross-seed/resources/config.js
@@ -0,0 +1,23 @@
+// Torrent content layout: Original
+// Default Torrent Management Mode: Automatic
+// Default Save Path: /media/Downloads/complete
+// Incomplete Save Path: /media/Downloads/incomplete
+
+module.exports = {
+  action: "inject",
+  apiKey: process.env.CROSS_SEED_API_KEY,
+  dataCategory: "cross-seed",
+  delay: 15,
+  duplicateCategories: true,
+  includeEpisodes: true,
+  includeNonVideos: true,
+  includeSingleEpisodes: true,
+  linkDir: "/media/Downloads/complete/cross-seed",
+  linkType: "hardlink",
+  matchMode: "safe",
+  outputDir: "/config",
+  qbittorrentUrl: "http://qbittorrent.media.svc.cluster.local:8080",
+  skipRecheck: true,
+  torrentDir: "/config/qBittorrent/BT_backup",
+  torznab: [], // Only using annoucements from autobrr
+};
