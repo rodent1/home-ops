51e1b88d5b251b00797ca21c8ae9c5617a75c0cf
Stian R. Sporaland <mail@stianrs.dev>
2025-06-28 10:14:24 +0200

    yoink some changes here and there - Bootstrap - Rook auto cleanup - Taskfiles

--- scripts/bootstrap-cluster.sh
+++ scripts/bootstrap-cluster.sh
@@ -117,7 +117,7 @@ function apply_crds() {
 
     local -r crds=(
         # renovate: datasource=github-releases depName=kubernetes-sigs/external-dns
-        https://raw.githubusercontent.com/kubernetes-sigs/external-dns/refs/tags/v0.18.0/docs/sources/crd/crd-manifest.yaml
+        https://raw.githubusercontent.com/kubernetes-sigs/external-dns/refs/tags/v0.18.0/config/crd/standard/dnsendpoints.externaldns.k8s.io.yaml
         # renovate: datasource=github-releases depName=kubernetes-sigs/gateway-api
         https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.3.0/experimental-install.yaml
         # renovate: datasource=github-releases depName=prometheus-operator/prometheus-operator
@@ -159,46 +159,9 @@ function apply_resources() {
     fi
 }
 
-# Disks in use by rook-ceph must be wiped before Rook is installed
-function wipe_rook_disks() {
-    log debug "Wiping Rook disks"
-
-    # Skip disk wipe if Rook is detected running in the cluster
-    # NOTE: Is there a better way to detect Rook / OSDs?
-    if kubectl --namespace rook-ceph get kustomization rook-ceph &>/dev/null; then
-        log warn "Rook is detected running in the cluster, skipping disk wipe"
-        return
-    fi
-
-    if ! nodes=$(talosctl config info --output json 2>/dev/null | jq --exit-status --raw-output '.nodes | join(" ")') || [[ -z "${nodes}" ]]; then
-        log error "No Talos nodes found"
-    fi
-
-    log debug "Talos nodes discovered" "nodes=${nodes}"
-
-    # Wipe disks on each node that match the ROOK_DISK environment variable
-    for node in ${nodes}; do
-        if ! disks=$(talosctl --nodes "${node}" get disk --output json 2>/dev/null |
-            jq --exit-status --raw-output --slurp '. | map(select(.spec.dev_path == env.ROOK_DISK) | .metadata.id) | join(" ")') || [[ -z "${nodes}" ]]; then
-            log error "No disks found" "node=${node}" "model=${ROOK_DISK}"
-        fi
-
-        log debug "Talos node and disk discovered" "node=${node}" "disks=${disks}"
-
-        # Wipe each disk on the node
-        for disk in ${disks}; do
-            if talosctl --nodes "${node}" wipe disk "${disk}" &>/dev/null; then
-                log info "Disk wiped" "node=${node}" "disk=${disk}"
-            else
-                log error "Failed to wipe disk" "node=${node}" "disk=${disk}"
-            fi
-        done
-    done
-}
-
-# Apply Helm releases using helmfile
-function apply_helm_releases() {
-    log debug "Applying Helm releases with helmfile"
+# Sync Helm releases
+function sync_helm_releases() {
+    log debug "Syncing Helm releases"
 
     local -r helmfile_file="${ROOT_DIR}/bootstrap/helmfile.yaml"
 
@@ -206,15 +169,15 @@ function apply_helm_releases() {
         log error "File does not exist" "file=${helmfile_file}"
     fi
 
-    if ! helmfile --file "${helmfile_file}" apply --hide-notes --skip-diff-on-install --suppress-diff --suppress-secrets; then
-        log error "Failed to apply Helm releases"
+    if ! helmfile --file "${helmfile_file}" sync --hide-notes; then
+        log error "Failed to sync Helm releases"
     fi
 
-    log info "Helm releases applied successfully"
+    log info "Helm releases synced successfully"
 }
 
 function main() {
-    check_env KUBECONFIG KUBERNETES_VERSION ROOK_DISK TALOS_VERSION
+    check_env KUBECONFIG KUBERNETES_VERSION TALOS_VERSION
     check_cli helmfile jq kubectl kustomize minijinja-cli op talosctl yq
 
     if ! op whoami --format=json &>/dev/null; then
@@ -228,10 +191,9 @@ function main() {
 
     # Apply resources and Helm releases
     wait_for_nodes
-    wipe_rook_disks
     apply_crds
     apply_resources
-    apply_helm_releases
+    sync_helm_releases
 
     log info "Congrats! The cluster is bootstrapped and Flux is syncing the Git repository"
 }
